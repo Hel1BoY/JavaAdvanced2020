4303676e7268f03dc6b1c686a1fb93be85bd9480
Vasil Manev <67638034+Hel1BoY@users.noreply.github.com>
2021-03-10 20:11:23 +0200

    Delete GameStore directory

--- GameStore/repo/OrderRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.GameStore.repo;
-
-import com.example.GameStore.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepo extends CrudRepository<Order, Long> {
-
-}
