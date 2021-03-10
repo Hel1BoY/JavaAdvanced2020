432dde55a7deadba880b85db2efaaee8c5bc0295
Vasil Manev <67638034+Hel1BoY@users.noreply.github.com>
2021-03-10 21:43:48 +0200

    Update UserServiceImpl.java

--- GameStore/src/main/java/com/example/GameStore/service/UserServiceImpl.java
+++ GameStore/src/main/java/com/example/GameStore/service/UserServiceImpl.java
@@ -30,6 +30,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) throws IllegalArgumentException{
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         checkProperties(user);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
