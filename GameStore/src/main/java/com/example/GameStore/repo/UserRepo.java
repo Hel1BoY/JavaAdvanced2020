d710e930df2c734a12ddb4367302298753ddd28e
Vasil Manev <67638034+Hel1BoY@users.noreply.github.com>
2021-03-10 21:33:17 +0200

    Update UserRepo.java

--- GameStore/src/main/java/com/example/GameStore/repo/UserRepo.java
+++ GameStore/src/main/java/com/example/GameStore/repo/UserRepo.java
@@ -9,7 +9,6 @@ public interface UserRepo extends CrudRepository<User,Long> {
 
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-    Optional<User> findById(Long id);
 
 
 }
